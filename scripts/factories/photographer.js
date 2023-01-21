7d9de04f6de05b666f856dfdc2c14c039eec3f63
Adrien Suignard <adrien.suignard@gmail.com>
2023-01-21 10:52:08 +0100

    page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  console.log(data);
 
-    const picture = `assets/photographers/${portrait}`;
+  const { name, portrait, tagline, price, city, country } = data;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    //Create elements
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const cityCountryElement = document.createElement("p");
+    cityCountryElement.innerText = `${city}, ${country}`;
+
+    const taglineElement = document.createElement("p");
+    taglineElement.innerText = `${tagline}`;
+
+    const priceElement = document.createElement("p");
+    priceElement.innerText = `${price}€/jour`;
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(cityCountryElement);
+    article.appendChild(taglineElement);
+    article.appendChild(priceElement);
+
+    return article;
+  }
+  return { name, picture, tagline, price, city, country, getUserCardDOM };
 }
